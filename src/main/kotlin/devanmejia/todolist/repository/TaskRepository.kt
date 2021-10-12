d9ff17c214450eebdcfb97ff9adf3238302c1fec
lyaha <lyah.artem10@mailru>
2021-10-12 23:48:25 +0300

    add transactional annotation

--- src/main/kotlin/devanmejia/todolist/repository/TaskRepository.kt
+++ src/main/kotlin/devanmejia/todolist/repository/TaskRepository.kt
@@ -2,9 +2,11 @@ package devanmejia.todolist.repository
 
 import devanmejia.todolist.model.Task
 import org.springframework.data.jpa.repository.JpaRepository
+import javax.transaction.Transactional
 
 interface TaskRepository : JpaRepository<Task, Long?> {
     fun findAllByUserLogin(login: String): List<Task>
     fun findByIdAndUserLogin(id: Long, login: String): Task?
+    @Transactional
     fun deleteByIdAndUserLogin(id: Long, login: String)
 }
