84c5ff13c196e6dc5b4c380ae4511e9a347a8dd7
lyaha <lyah.artem10@mailru>
2021-10-12 23:24:32 +0300

    implement user controller

--- src/main/kotlin/devanmejia/todolist/controller/UserController.kt
+++ src/main/kotlin/devanmejia/todolist/controller/UserController.kt
@@ -1,9 +1,19 @@
 package devanmejia.todolist.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import devanmejia.todolist.service.user.UserService
+import devanmejia.todolist.transfer.UserDTO
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/user")
-class UserController {
+@RequestMapping("/api")
+class UserController(
+    private val userService: UserService) {
+
+    @PostMapping("/user")
+    fun createNewUser(@RequestBody userDTO: UserDTO) =
+        UserDTO.from(userService.createNewUser(userDTO))
+
+    @PatchMapping("/user/{login}/login/{newLogin}")
+    fun renameUser(@PathVariable login: String, @PathVariable newLogin: String) =
+        UserDTO.from(userService.renameUser(login, newLogin))
 }
