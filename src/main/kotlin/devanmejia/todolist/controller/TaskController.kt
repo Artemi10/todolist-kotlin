704e6b722c949b0f9eb9020effef73699d09fc74
lyaha <lyah.artem10@mailru>
2021-10-13 19:16:13 +0300

    update controllers

--- src/main/kotlin/devanmejia/todolist/controller/TaskController.kt
+++ src/main/kotlin/devanmejia/todolist/controller/TaskController.kt
@@ -2,37 +2,49 @@ package devanmejia.todolist.controller
 
 import devanmejia.todolist.model.Content
 import devanmejia.todolist.service.task.TaskService
+import devanmejia.todolist.service.token.TokenService
 import devanmejia.todolist.service.user.UserService
 import devanmejia.todolist.transfer.TaskDTO
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/api")
 class TaskController(
+    private val tokenService: TokenService,
     private val taskService: TaskService,
     private val userService: UserService) {
 
-    @GetMapping("/tasks/user/{login}")
-    fun getAllUsersTasks(@PathVariable login: String) =
-        TaskDTO.from(taskService.getAllUsersTasks(login))
+    @GetMapping("/tasks")
+    fun getAllUsersTasks(request: HttpServletRequest): List<TaskDTO>{
+        val login = tokenService.extractUserLogin(request)
+        return TaskDTO.from(taskService.getAllUsersTasks(login))
+    }
 
-    @PostMapping("/task/user/{login}")
-    fun createNewTask(@PathVariable login: String, @RequestBody taskDTO: TaskDTO): TaskDTO{
+    @PostMapping("/task")
+    fun createNewTask(@RequestBody taskDTO: TaskDTO, request: HttpServletRequest): TaskDTO{
+        val login = tokenService.extractUserLogin(request)
         val user = userService.getUserByLogin(login)
         return TaskDTO.from(taskService.createNewTask(taskDTO, user))
     }
 
-    @DeleteMapping("/task/{id}/user/{login}")
-    fun deleteTask(@PathVariable("id") taskId: Long, @PathVariable login: String) =
+    @DeleteMapping("/task/{id}")
+    fun deleteTask(@PathVariable("id") taskId: Long, request: HttpServletRequest){
+        val login = tokenService.extractUserLogin(request)
         taskService.removeTask(taskId, login)
+    }
 
-    @PatchMapping("/task/{id}/close/user/{login}")
-    fun closeTask(@PathVariable("id") taskId: Long, @PathVariable login: String) =
-        TaskDTO.from(taskService.closeTask(taskId, login))
+    @PatchMapping("/task/{id}/close")
+    fun closeTask(@PathVariable("id") taskId: Long, request: HttpServletRequest): TaskDTO{
+        val login = tokenService.extractUserLogin(request)
+        return TaskDTO.from(taskService.closeTask(taskId, login))
+    }
 
-    @PatchMapping("/task/{id}/content/user/{login}")
+    @PatchMapping("/task/{id}/content")
     fun changeContent(@PathVariable("id") taskId: Long,
-                      @PathVariable login: String, @RequestBody content: Content) =
-        TaskDTO.from(taskService.changeContent(taskId, content, login))
+                      @RequestBody content: Content, request: HttpServletRequest): TaskDTO{
+        val login = tokenService.extractUserLogin(request)
+        return TaskDTO.from(taskService.changeContent(taskId, content, login))
+    }
 
 }
--- src/main/kotlin/devanmejia/todolist/controller/UserController.kt
+++ src/main/kotlin/devanmejia/todolist/controller/UserController.kt
@@ -1,19 +1,25 @@
 package devanmejia.todolist.controller
 
+import devanmejia.todolist.service.token.TokenService
 import devanmejia.todolist.service.user.UserService
 import devanmejia.todolist.transfer.UserDTO
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/api")
 class UserController(
+    private val tokenService: TokenService,
     private val userService: UserService) {
 
     @PostMapping("/user")
     fun createNewUser(@RequestBody userDTO: UserDTO) =
         UserDTO.from(userService.createNewUser(userDTO))
 
-    @PatchMapping("/user/{login}/login/{newLogin}")
-    fun renameUser(@PathVariable login: String, @PathVariable newLogin: String) =
-        UserDTO.from(userService.renameUser(login, newLogin))
+    @PatchMapping("/user/login/{login}")
+    fun renameUser(@PathVariable login: String, request: HttpServletRequest) : UserDTO{
+        val oldLogin = tokenService.extractUserLogin(request)
+        return UserDTO.from(userService.renameUser(oldLogin, login))
+    }
+
 }
