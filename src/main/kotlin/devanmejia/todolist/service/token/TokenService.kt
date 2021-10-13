71581a69aef08d177d1c07949b0eb510ca5a407f
lyaha <lyah.artem10@mailru>
2021-10-13 19:16:33 +0300

    update token service

--- src/main/kotlin/devanmejia/todolist/service/token/TokenService.kt
+++ src/main/kotlin/devanmejia/todolist/service/token/TokenService.kt
@@ -1,9 +1,10 @@
 package devanmejia.todolist.service.token
 
-import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.http.server.ServerHttpRequest
 import org.springframework.stereotype.Service
+import javax.servlet.http.HttpServletRequest
 
 @Service
 interface TokenService {
-    fun extractUserLogin(request: ServerHttpRequest): String
+    fun extractUserLogin(request: HttpServletRequest): String
 }
--- src/main/kotlin/devanmejia/todolist/service/token/TokenServiceImpl.kt
+++ src/main/kotlin/devanmejia/todolist/service/token/TokenServiceImpl.kt
@@ -1,14 +1,14 @@
 package devanmejia.todolist.service.token
 
 import devanmejia.todolist.exception.AuthorizationException
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Service
+import javax.servlet.http.HttpServletRequest
 
 @Service
 class TokenServiceImpl : TokenService {
 
-    override fun extractUserLogin(request: ServerHttpRequest) =
-        request.headers["Authorization"]?.get(0)
+    override fun extractUserLogin(request: HttpServletRequest) =
+        request.getHeader("Authorization")
             ?: throw AuthorizationException("Token is not provided")
 
 }
